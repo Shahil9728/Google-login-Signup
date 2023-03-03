0cc131d8237c0d757b8de7352e56f1eaca5cf4d2
Shahil Kumar <96991517+Shahil9728@users.noreply.github.com>
2023-03-03 17:54:54 +0530

    clo

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ function onSignIn(googleUser) {
 
     console.log("hello");
 
-    $(".data").css("display","block");
+    // $(".data").css("display","block");
     $(`#name`).text(profile.getName());
     $(`#email`).text(profile.getEmail());
     $(`#image`).attr('src',profile.getImageUrl());
    
-    $(".g-signin2").css("display","none");
+    // $(".g-signin2").css("display","none");
     
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getName());
@@ -20,8 +20,8 @@ function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
         alert('You have been signed out successfully');
-        $(".data").css("display","none");
-        $(".g-signin2").css("display","block");
+        // $(".data").css("display","none");
+        // $(".g-signin2").css("display","block");
     });
   }
 
